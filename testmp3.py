28ecba8204fa05ed8de72f412dfd9d86e9cf2a51
Consumer Banking <cb@Consumers-MacBook-Pro.local>
2019-05-21 11:21:42 +0530

    Mp3 support for enrollment and authentication

--- testmp3.py
+++ testmp3.py
@@ -1,10 +1,11 @@
 import subprocess
 import os
-
+"""
 from pydub import AudioSegment
 
 from pydub import AudioSegment
 from pydub.utils import which
+
 AudioSegment.converter = './ffmpeg/ffmpeg'
 #AudioSegment.ffmpeg = "/Users/cb/ffmpeg"
 #print("************** ",AudioSegment.converter)
@@ -12,3 +13,6 @@ AudioSegment.converter = './ffmpeg/ffmpeg'
 #print("ABC   ",AudioSegment.converter)
 #print("****** " ,os.environ)
 subprocess.call([AudioSegment.converter, '-i', 'aryan.mp3', 'output.wav'])
+"""
+pipeline = gst.parse_launch("/Users/cb/voice-recognition/voice-recognition/Aryan.wav ! decodebin ! audioconvert !  lame ! filesink location=/Users/cb/voice-recognition/voice-recognition/Aryan.mp3")
+pipeline.set_state(gst.STATE_PLAYING)
